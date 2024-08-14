d8b1a38e8bea2d3ea5197b357fa961ec7422f2e9
Aarti Kumari <119749450+aarti12217444@users.noreply.github.com>
2024-08-14 18:41:12 +0530

    Add files via upload

---
+++ valvar.kt
@@ -0,0 +1,17 @@
+
+fun main() {
+    var a:Int=89
+    print("The value of a is:"+ a)
+    
+    a = 90
+    print(a)
+}
+//var is mutable so here 89 and 90 is coming but val is immutable so if we assign another
+//value of a in val then it will give error
+
+// fun main() {
+//     val a:Int=89
+//     print("The value of a is:"+ a)
+// }
+
+
